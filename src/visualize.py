0ff043a3a0a87ff5c2704fdba796e9489d0f8b54
Mihir Bafna <mihirb14@imp.csail.mit.edu>
2023-06-21 14:25:36 -0400

    added visualize.py

---
+++ src/visualize.py
@@ -0,0 +1,12 @@
+import plotly.express as px
+import plotly.graph_objects as go
+import seaborn as sns
+
+
+def visualize_contact_map(map, zmax):
+    fig = px.imshow(map.squeeze(),zmax=zmax,color_continuous_scale="darkmint", width=500)
+
+    fig.update_layout(
+        margin=dict(l=0, r=0, t=0, b=0),
+    )
+    return fig    
