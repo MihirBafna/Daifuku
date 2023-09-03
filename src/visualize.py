b9f6b6bce903695c1a8a6ce5b8ebac3846cb8771
Mihir Bafna <mihirb14@imp.csail.mit.edu>
2023-09-03 15:14:16 -0400

    made visualizations modular

--- src/visualize.py
+++ src/visualize.py
@@ -1,13 +1,21 @@
 import plotly.express as px
 import plotly.graph_objects as go
 import seaborn as sns
+import ipywidgets as widgets
 
 
-def visualize_contact_map(map, zmax):
+def visualize_contact_map(map, zmax, title=""):
     fig = px.imshow(map.squeeze(),zmax=zmax,color_continuous_scale="darkmint", width=500)
 
     fig.update_layout(
-        margin=dict(l=0, r=0, t=0, b=0),
+        margin=dict(l=0, r=0, t=50, b=0),
+        title=title,
     )
     return fig
 
+def visualize_multiple_jupyter(maps):
+    figurewidgets= []
+    for i in range(len(maps)):
+        figurewidgets.append(go.FigureWidget(maps[i]))
+
+    return widgets.HBox(figurewidgets)
